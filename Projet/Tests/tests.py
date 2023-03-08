cec50c6f8b0973d6d5e068315fc9aee814d5a04b
ArthurDrd <arthurdurand.pro1@gmail.com>
2023-03-08 16:45:08 -0500

    modified:   Projet/Tests/tests.py

--- Projet/Tests/tests.py
+++ Projet/Tests/tests.py
@@ -6,3 +6,9 @@ def create_order(client):
         }
     })
     assert response.status_code == 302
+
+class TestGetOrder:
+    def test_get_order(self, client):
+        create_order(client)
+        response = client.get('/order/1')
+        assert response.status_code == 200
